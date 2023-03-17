e4e24437dce39f2d49644b008bf13e83925d75f0
Azharabdurrahman <103914948+Azharabdurrahman@users.noreply.github.com>
2023-03-17 18:37:45 +0800

    Delete PRAK101.php

--- PRAK101.php
+++
@@ -1,9 +0,0 @@
-
-        <!--- PHP--->
-        <?php
-        echo "Hello World in PHP </br>";
-        $nama = "Muhammad Azhar Abdurrahman";
-        echo  "Nama Praktikan : $nama </br>";
-        $nim = "2110817310014";
-        echo "NIM : $nim"; 
-        ?>
